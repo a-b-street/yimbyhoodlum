75e9e2018a422c441008755e297a980766101dfc
Dustin Carlino <dabreegster@gmail.com>
2021-08-17 13:59:21 -0700

    Increase JSON blob size; 16KB was exceeded by quite modest edits

--- proposals.sql
+++ proposals.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS proposals (
 	id VARCHAR(36) PRIMARY KEY,
 	-- In the "us/seattle/udistrict" form
 	map_name TEXT NOT NULL CHECK(length(map_name) < 50),
-	-- The MapEdits JSON blob
-	json BLOB NOT NULL,
+	-- The MapEdits JSON blob, max 16MB
+	json MEDIUMBLOB NOT NULL,
 	-- An enum state machine
 	-- 0 = awaiting moderation,
 	-- 1 = public
